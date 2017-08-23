68136270efdae91d57135e8ee2a51e8f70dd1830
zhangyangyang <admin@LAPTOP-5V8AISB8>
2017-08-23 17:42:18 +0800

    Somates

---
+++ src/com/program/somate/config/SoMateConfig.java
@@ -0,0 +1,42 @@
+package com.program.somate.config;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+public class SoMateConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
